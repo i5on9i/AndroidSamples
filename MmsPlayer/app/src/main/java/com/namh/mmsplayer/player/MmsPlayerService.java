d7524d140b6b34914877c135746dd4b13277ccff
i5on9i <gaedduck@gmail.com>
2015-01-08 22:15:57 +0900

    - add service

---
+++ MmsPlayer/app/src/main/java/com/namh/mmsplayer/player/MmsPlayerService.java
@@ -0,0 +1,32 @@
+package com.namh.mmsplayer.player;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.Binder;
+import android.os.IBinder;
+
+/**
+ * Created by namh on 2015-01-08.
+ */
+public class MmsPlayerService extends Service {
+
+    // This is the object that receives interactions from clients.  See
+    // RemoteService for a more complete example.
+    private final IBinder mBinder = new LocalBinder();
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return mBinder;
+    }
+
+    /**
+     * Class for clients to access.  Because we know this service always
+     * runs in the same process as its clients, we don't need to deal with
+     * IPC.
+     */
+    public class LocalBinder extends Binder {
+        MmsPlayerService getService() {
+            return MmsPlayerService.this;
+        }
+    }
+}
