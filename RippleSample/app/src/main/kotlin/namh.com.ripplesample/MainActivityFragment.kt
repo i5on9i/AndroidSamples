c8691d480d73eca6d90f78bb46790a57516e82b7
i5on9i <gaedduck@gmail.com>
2015-11-26 16:52:48 +0900

    - update for gradle 1.3.+

---
+++ RippleSample/app/src/main/kotlin/namh.com.ripplesample/MainActivityFragment.kt
@@ -0,0 +1,17 @@
+package namh.com.ripplesample
+
+import android.support.v4.app.Fragment
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class MainActivityFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        return inflater!!.inflate(R.layout.fragment_main, container, false)
+    }
+}
